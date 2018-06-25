356d09de9658950d85de494ec36d5a6dbde16d75
Timothy GREIL <tgreil@dump-pedago-2.42.fr>
2018-06-25 13:00:09 +0200

    compil ok xd

--- srcs/ft_ls_apply.c
+++ srcs/ft_ls_apply.c
@@ -6,7 +6,7 @@
 /*   By: tgreil <tgreil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 12:50:17 by tgreil            #+#    #+#             */
-/*   Updated: 2018/06/25 12:58:01 by tgreil           ###   ########.fr       */
+/*   Updated: 2018/06/25 12:59:54 by tgreil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int		ft_ls_apply_f(t_list_manag *list, t_list_ls *elem)
 {
-	if (elem->stat->st_mode & S_IFDIR)
+	if (elem->stat.st_mode & S_IFDIR)
+	{
+		ft_printf("DIR\n");
+	}
+	list += 1;
 	return (E_SUCCESS);
 }
 
